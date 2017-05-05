912b20c3caa7125a3d2eb6c2d55a8cbb30bbd21b
reyemDarnok <ben.k.meyer@gmail.com>
2017-05-05 09:09:21 +0200

    Update Dot.java
    added commentary

--- PACMAN/Dot.java
+++ PACMAN/Dot.java
@@ -8,6 +8,7 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class Dot extends Actor
 {
+    //describes whether this dot is a simple dot or one of various powerups
     public int state;
     /**
      * Act - do whatever the Dot wants to do. This method is called whenever
