c6f9f5ea1695995c8c2cfc2adce1f78c9fbf7899
reyemDarnok <ben.k.meyer@gmail.com>
2017-05-08 08:11:37 +0200

    Update PACMANWorld.java
    added comments

--- PACMAN/PACMANWorld.java
+++ PACMAN/PACMANWorld.java
@@ -8,7 +8,9 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class PACMANWorld extends World
 {
+    //the score of the player
     private int score;
+    //the number of dots left(if zero, start next world)
     private int dots;
     /**
      * Constructor for objects of class MyWorld.
@@ -19,6 +21,10 @@ public class PACMANWorld extends World
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(600, 400, 1); 
     }
+    /**
+        removes a dot. Does not increase score.
+        @param eaten the dot to be eaten
+    **/
     public void removeDot(Dot eaten)
     {
         removeObject(eaten);
