547dc1823863f8caa78269be03853d173c9857f6
reyemDarnok <ben.k.meyer@gmail.com>
2017-06-26 16:45:15 +0200

    Update Dot.java
    added state Randomization

--- PACMAN/Dot.java
+++ PACMAN/Dot.java
@@ -10,6 +10,15 @@ public class Dot extends Actor
 {
     //describes whether this dot is a simple dot or one of various powerups
     public int state;
+    
+    public void addedToWorld(World world)
+    {
+        state=Greenfoot.getRandomNumber(25);
+        if(state==21)
+        {
+            //setImage(non-Default);
+        }
+    }
     /**
      * Act - do whatever the Dot wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
