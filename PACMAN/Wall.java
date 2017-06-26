2de8f0c00b3aa30f4603dd0f36cc6b2c15fb0240
reyemDarnok <ben.k.meyer@gmail.com>
2017-06-26 18:57:25 +0200

    Commented Wall.java

--- PACMAN/Wall.java
+++ PACMAN/Wall.java
@@ -1,19 +1,10 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * Write a description of class Wall here.
+ * It stands around. That's all
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * @author (Ben Konrad Meyer) 
+ * @version (2017-06-26)
  */
 public class Wall extends Actor
-{
-    /**
-     * Act - do whatever the Wall wants to do. This method is called whenever
-     * the 'Act' or 'Run' button gets pressed in the environment.
-     */
-    public void act() 
-    {
-        // Add your action code here.
-    }    
-}
+{}
